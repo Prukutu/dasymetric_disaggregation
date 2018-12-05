e4c66f0813ac01f5a1b3a8c2d578c1f8293c5d8c
prukutu <luis.ortiz.uriarte@gmail.com>
2018-12-05 15:37:24 -0500

    update class name

--- tests/tests.py
+++ tests/tests.py
@@ -6,14 +6,20 @@ import geopandas
 sys.path.append('/home/luis/github/dasymetric_disaggregation/')
 import dasymetry
 
-dasy = dasymetry.Dasymetry()
-parcel_df = dasy.load_parcels('SIMapPLUTO.shp')
+dasy = dasymetry.DasymetryDisaggregate()
+datadir = '/home/luis/github/dasymetric_disaggregation/testdata/'
 
 # Do a few tests on our data loading methods
 class TestDataLoadMethods(unittest.TestCase):
 
     def test_parcel_df_type(self):
-        self.assertEqual(type(parcel_df), geopandas.GeoDataFrame)
+        parcel_df = dasy.load_parcels(datadir + 'SIMapPLUTO.shp')
+        self.assertIs(type(parcel_df), geopandas.GeoDataFrame)
+
+    def test_source_df_type(self):
+        source_df = dasy.load_source_data(datadir + 'tabblock2010_36_pophu.shp')
+        self.assertIs(type(source_df), geopandas.GeoDataFrame)
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDataLoadMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
